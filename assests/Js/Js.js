b630557c645c6b40d42e6ed58e5a73cb670a04fd
Fábio Nogueira Asseiro <fabioasseiro@hotmail.com>
2023-10-27 12:52:05 -0300

    att

--- assests/Js/Js.js
+++ assests/Js/Js.js
@@ -3,13 +3,13 @@ const body = document.body;
 const img = document.getElementById('img-principal');
 
 mode.addEventListener('click', () => {
-    if (body.classList.contains('dark-mode')) {
-        body.classList.remove('dark-mode');
+    if (body.classList.contains('light-mode')) {
+        body.classList.remove('light-mode');
         mode.classList.remove('fa-sun');
         mode.classList.add('fa-moon');
         img.src = "assests/img/img-dark.svg";
     } else {
-        body.classList.add('dark-mode');
+        body.classList.add('light-mode');
         mode.classList.remove('fa-moon');
         mode.classList.add('fa-sun');
         img.src = "assests/img/img-light.svg";
